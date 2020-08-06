3a35e0d13716a1495a27701c6491817f13670f45
Anmol Sahoo <anmol.sahoo25@gmail.com>
2020-08-06 14:43:18 +0530

    updated

--- src/scylla.ml
+++ src/scylla.ml
@@ -4,6 +4,9 @@ type t = {ic : in_channel ; oc : out_channel}
 
 type value = Protocol.value
 
+let cql_version_key = Bigstringaf.of_string "CQL_VERSION" ~off:0 ~len:11
+let cql_version_val = Bigstringaf.of_string "3.3.1" ~off:0 ~len:5
+
 let serialize_to_bytes s =
   let buffers = List.map Faraday.serialize_to_bigstring s in
   let lens = List.map Bigstringaf.length buffers in
@@ -19,7 +22,7 @@ let serialize_to_bytes s =
 let connect ~ip:ip ~port:port =
   let addr = Unix.ADDR_INET (Unix.inet_addr_of_string ip, port) in
   let (ic, oc) = Unix.open_connection addr in
-  let startup = Protocol.(Req { flags = [] ; stream = 0; op = Startup ; body = Empty }) in
+  let startup = Protocol.(Req { flags = [] ; stream = 0; op = Startup ; body = Map [(cql_version_key, cql_version_val)]}) in
   let serializers = Serialize.(to_bytes startup) in
   let buffer = serialize_to_bytes serializers in
   output_bytes oc buffer;
